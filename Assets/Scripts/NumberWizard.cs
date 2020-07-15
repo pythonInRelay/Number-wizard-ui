df96b3ce8839fb55e075a410c3eec7082f4275f8
pythoninrelay <S4F4gEEnzT#zxy6Eos>
2020-07-15 13:07:02 +0300

    Add files

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] int minGuess;
+    [SerializeField] int maxGuess;
+    [SerializeField] private TextMeshProUGUI guessText;
+    int _guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+        maxGuess += 1;
+    }
+
+    public void OnPressHigher()
+    {
+        minGuess = _guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        maxGuess = _guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        _guess = Random.Range(minGuess, maxGuess);
+        guessText.text = _guess.ToString();
+    }
+}
